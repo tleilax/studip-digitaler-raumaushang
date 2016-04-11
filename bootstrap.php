831276e27e47dc49e35a9d3374440b833b6520bf
Jan-Hendrik Willms <jan.hendrik.willms@uni-oldenburg.de>
2016-04-11 12:04:43 +0200

    Fix fatal error

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'vendor/trails/trails.php';
     require_once 'app/controllers/studip_controller.php';
+    require_once 'app/controllers/plugin_controller.php';
 
     StudipAutoloader::addAutoloadPath(__DIR__ . '/classes');
     StudipAutoloader::addAutoloadPath(__DIR__ . '/classes', 'Raumaushang');
