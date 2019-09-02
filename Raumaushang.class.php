d9c63f57d3a7af9fa7657800ea535fbdf232929c
Jan-Hendrik Willms <jan.hendrik.willms@uni-oldenburg.de>
2019-09-02 13:49:38 +0200

    Studip 4.3

--- Raumaushang.class.php
+++ Raumaushang.class.php
@@ -41,7 +41,15 @@ class Raumaushang extends StudIPPlugin implements SystemPlugin
 
     public function addJS($asset)
     {
-        PageLayout::addScript($this->getPluginURL() . '/' . ltrim($asset, '/'));
+        static $manifest = null;
+        if ($manifest === null) {
+            $manifest = $this->getMetadata();
+        }
+
+        PageLayout::addScript(URLHelper::getURL(
+            $this->getPluginURL() . '/' . ltrim($asset, '/'),
+            ['v' => $manifest['version']]
+        ));
     }
 
     // This is still ugly, since it copies almost all of the core functionality
