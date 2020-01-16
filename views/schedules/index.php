e33b3c515a3a0466d8633fc0dd8ad352773dfbb9
David Siegfried <ds.siegfried@gmail.com>
2020-01-16 11:53:36 +0100

    revert changes, re #29

--- views/schedules/index.php
+++ views/schedules/index.php
@@ -2,7 +2,7 @@
     <? foreach ($resources as $resource): ?>
         <li>
             <a href="<?= $controller->url_for('schedules/building/' . $resource->id) ?>">
-                <?= htmlReady($resource->parent->name) ?> / <?= htmlReady($resource->name) ?>
+                <?= htmlReady($resource->name) ?>
             </a>
         </li>
     <? endforeach; ?>
