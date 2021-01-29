b92a9c5ef6e949f374f90ec3a559236789515346
Jan-Hendrik Willms <tleilax+github@gmail.com>
2021-01-29 15:36:44 +0100

    re #81

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,3 @@
 <?php
-    require_once 'vendor/trails/trails.php';
-    require_once 'app/controllers/studip_controller.php';
-    require_once 'app/controllers/plugin_controller.php';
-
     StudipAutoloader::addAutoloadPath(__DIR__ . '/lib', 'Raumaushang');
     StudipAutoloader::addAutoloadPath(__DIR__ . '/models', 'Raumaushang');
